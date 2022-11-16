9e7574caf467de3c4c9b5c21436d44415b5e34b7
Ezekiel Victor <codeepoch@gmail.com>
2022-11-16 06:54:07 +0100

    changed allowd host to use env variables

--- oil/settings.py
+++ oil/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS = HOSTS.split(',')
 
 
 # Application definition
